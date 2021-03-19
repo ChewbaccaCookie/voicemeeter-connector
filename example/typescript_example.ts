2aebf35c90ea7f4a3f0126ab96f0a2fd6e0fcd79
Jonathan Bußhoff <jonathan@busshoff.net>
2021-03-19 23:49:27 +0100

    docu generation added to publish workflow

--- example/typescript_example.ts
+++ example/typescript_example.ts
@@ -17,6 +17,10 @@ Voicemeeter.init().then(async (vm) => {
 	// Get random gain of strip 0
 	console.log(vm.getStripParameter(0, StripProperties.Gain));
 
+	vm.attachChangeEvent(() => {
+		console.log("Something changed!");
+	});
+
 	// Disconnect voicemeeter client
-	setTimeout(vm.disconnect, 500);
+	setTimeout(vm.disconnect, 5000);
 });
